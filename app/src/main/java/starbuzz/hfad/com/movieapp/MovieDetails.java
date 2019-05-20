649c6fb7caec680dcb7a061188af39352886b437
Kaila <kailajones431@gmail.com>
2019-05-20 09:41:26 -0700

    GridView Adapter

--- app/src/main/java/starbuzz/hfad/com/movieapp/MovieDetails.java
+++ app/src/main/java/starbuzz/hfad/com/movieapp/MovieDetails.java
@@ -1,7 +1,49 @@
 package starbuzz.hfad.com.movieapp;
 
-import java.util.ArrayList;
-
 public class MovieDetails {
+    private String Cast;
+    private String Crew;
+    private String Trailers;
+    private String Reviews;
+    private String SimilarMovies;
 
+    public String getCast() {
+        return Cast;
+    }
+
+    public void setCast(String cast) {
+        Cast = cast;
+    }
+
+    public String getCrew() {
+        return Crew;
+    }
+
+    public void setCrew(String crew) {
+        Crew = crew;
+    }
+
+    public String getTrailers() {
+        return Trailers;
+    }
+
+    public void setTrailers(String trailers) {
+        Trailers = trailers;
+    }
+
+    public String getReviews() {
+        return Reviews;
+    }
+
+    public void setReviews(String reviews) {
+        Reviews = reviews;
+    }
+
+    public String getSimilarMovies() {
+        return SimilarMovies;
+    }
+
+    public void setSimilarMovies(String similarMovies) {
+        SimilarMovies = similarMovies;
+    }
 }
